26850c9571310194e7edfc986b8e1ff36bfb8f2e
mohamad <46373929+mohamadiarch@users.noreply.github.com>
2023-12-06 15:31:38 +0000

    Update bufferline.lua
    ordinal tabs

--- lua/lvim/core/bufferline.lua
+++ lua/lvim/core/bufferline.lua
@@ -57,7 +57,7 @@ M.config = function()
     },
     options = {
       mode = "buffers", -- set to "tabs" to only show tabpages instead
-      numbers = "none", -- can be "none" | "ordinal" | "buffer_id" | "both" | function
+      numbers = "ordinal", -- can be "none" | "ordinal" | "buffer_id" | "both" | function
       close_command = function(bufnr) -- can be a string | function, see "Mouse actions"
         M.buf_kill("bd", bufnr, false)
       end,
